5794b122c35368ddf4fa7344c3c3d91912e9feaf
alpertanrikulu <tanrikulualper77100@gmail.com>
2025-02-25 23:15:08 +0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
+import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
 import "./App.scss";
 
 function App() {
